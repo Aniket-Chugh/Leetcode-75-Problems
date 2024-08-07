e880752262c2e8a60731073baf2b0a3dcd992080
Aniket Chugh <149312276+Aniket-Chugh@users.noreply.github.com>
2024-08-08 00:09:45 +0530

    Update 05_Problems5.java

--- Arrays & String /05_Problems5.java
+++ Arrays & String /05_Problems5.java
@@ -32,6 +32,6 @@ class problem5 {
 
 	public static void main(String[] args) {
 		String str = "hello world";
-		System.out.println(reverseVowel(str));
+		System.out.println(reverseVowels(str));
 	}
 } 
